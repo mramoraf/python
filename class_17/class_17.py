57f7300d07e14ba66ca3cbce8e434fb550061425
Taisia <gorobetstaisia9@gmail.com>
2023-03-18 09:42:08 +0100

    class 17 hw

--- class_17/class_17.py
+++
@@ -1,7 +0,0 @@
-def reverse_string(string):
-    reversed_string = ''
-    for char in string:
-       reversed_string = char + reversed_string
-    return reversed_string
-
-print(reverse_string('abcde'))
